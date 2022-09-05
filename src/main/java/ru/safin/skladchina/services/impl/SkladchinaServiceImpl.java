e7e3a95895cd5a6866a34ed53d4e4bf902cd2c71
Maksim D Safin <crockolaco@yandex.ru>
2022-09-05 21:53:02 +0500

    feat: skladchina service impl add create method

--- src/main/java/ru/safin/skladchina/services/impl/SkladchinaServiceImpl.java
+++ src/main/java/ru/safin/skladchina/services/impl/SkladchinaServiceImpl.java
@@ -7,8 +7,11 @@ import ru.safin.skladchina.entities.Skladchina;
 import ru.safin.skladchina.entities.User;
 import ru.safin.skladchina.exceptions.BusinessException;
 import ru.safin.skladchina.repositories.SkladchinaRepository;
+import ru.safin.skladchina.repositories.UserRepository;
 import ru.safin.skladchina.services.AbstractService;
 import ru.safin.skladchina.services.SkaldchinaService;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -16,9 +19,16 @@ public class SkladchinaServiceImpl
         extends AbstractService<Skladchina, SkladchinaRepository> implements SkaldchinaService {
 
     SkladchinaProperties skladchinaProperties;
+    UserRepository userRepository;
 
-    public SkladchinaServiceImpl(SkladchinaRepository repository) {
+    public SkladchinaServiceImpl(
+            SkladchinaRepository repository,
+            SkladchinaProperties skladchinaProperties,
+            UserRepository userRepository
+            ) {
         super(repository);
+        this.skladchinaProperties = skladchinaProperties;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -26,13 +36,31 @@ public class SkladchinaServiceImpl
         Skladchina skladchina = this.get(skladchinaId);
         log.info("add participant {} to skladchina {}", user, skladchina);
 
-        if (skladchina.getParticipantsCount() >= skladchinaProperties.getMaxParticipants()) {
+        int participantCount = skladchina.getParticipantsCount();
+
+        if (participantCount >= skladchinaProperties.getMaxParticipants()) {
             throw BusinessException.create("Max participants count reached");
         }
 
         skladchina.addParticipant(user);
+        skladchina.setParticipantsCount(participantCount + 1);
 
         this.update(skladchina);
+    }
+
+    @Override
+    public Skladchina create(Skladchina skladchina, String userId) {
+
+        User user = userRepository.findById(userId).orElseThrow(() ->
+                BusinessException.create(
+                        String.format("No user with id=%s found to create skladchina", userId)
+                )
+        );
+
+
+        skladchina.setCreator(user);
+
+        return super.create(skladchina);
     }
 
 
