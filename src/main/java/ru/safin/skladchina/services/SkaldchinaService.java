2061f0c175673983b00157b5f69f2c703e2b55a2
Maksim D Safin <crockolaco@yandex.ru>
2022-09-05 21:52:49 +0500

    fix: skladchina serivce

--- src/main/java/ru/safin/skladchina/services/SkaldchinaService.java
+++ src/main/java/ru/safin/skladchina/services/SkaldchinaService.java
@@ -3,8 +3,8 @@ package ru.safin.skladchina.services;
 import ru.safin.skladchina.entities.Skladchina;
 import ru.safin.skladchina.entities.User;
 
-import java.util.List;
-
-public interface SkaldchinaService {
+public interface SkaldchinaService extends CommonService<Skladchina> {
     void addParticipant(User user, String skladchinaId);
+
+    Skladchina create(Skladchina skladchina, String userId);
 }
